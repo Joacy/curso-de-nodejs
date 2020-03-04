ae3f1ed58e88d329defd1ef1197cf11799045053
JoacyMS <joacymsilva@gmail.com>
2020-03-04 16:43:13 -0300

    Organização dos diretórios e adição da rota de 'get' de produtos

---
+++ node-api/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://joacyms:bancomongojoacy13@cluster0-355hd.mongodb.net/nodeapi?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+require('./src/models/Product');
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);

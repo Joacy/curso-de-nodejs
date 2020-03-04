3becc1290970c225e4765121bb7caee292e8d698
JoacyMS <joacymsilva@gmail.com>
2020-03-04 16:55:24 -0300

    Adição da rota de 'post' de produtos

--- node-api/server.js
+++ node-api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+app.use(express.json());
 
 mongoose.connect('mongodb+srv://joacyms:bancomongojoacy13@cluster0-355hd.mongodb.net/nodeapi?retryWrites=true&w=majority', {
     useNewUrlParser: true,

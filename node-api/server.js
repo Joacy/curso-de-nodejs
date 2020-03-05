0f2d4edbf50ccc01097e30298348a23b78b7ef68
JoacyMS <joacymsilva@gmail.com>
2020-03-05 17:24:17 -0300

    Adição do CORS, para que outros endereços acessem a api

--- node-api/server.js
+++ node-api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect('mongodb+srv://joacyms:bancomongojoacy13@cluster0-355hd.mongodb.net/nodeapi?retryWrites=true&w=majority', {
     useNewUrlParser: true,
